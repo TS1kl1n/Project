93921fa2cf77bfe53f40c072f7159a42b3882d3e
Ruslan Mukminov <muk-ruslan@mail.ru>
2024-06-17 11:50:10 +0300

    fix_bag_pdf_to_png.py

--- converters/pdf_to_image.py
+++ converters/pdf_to_image.py
@@ -1,13 +1,22 @@
 from .base_converter import BaseConverter
-from pdf2image import convert_from_path
+import fitz
 import os
 
 class PDFToImageConverter(BaseConverter):
     def convert(self):
-        images = convert_from_path(self.input_path)
+        pdf_document = fitz.open(self.input_path)
+        
         output_paths = []
-        for i, image in enumerate(images):
-            output_path = f"{os.path.splitext(self.input_path)[0]}_page_{i + 1}.png"
-            image.save(output_path, 'PNG')
+        
+        for page_num in range(len(pdf_document)):
+            page = pdf_document.load_page(page_num)
+            
+            pix = page.get_pixmap()
+            
+            output_path = f"{os.path.splitext(self.input_path)[0]}_page_{page_num + 1}.png"
+            
+            pix.save(output_path)
+            
             output_paths.append(output_path)
+        
         return output_paths[0] if len(output_paths) == 1 else output_paths
